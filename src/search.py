16cfb29967326c85f9e91647790dd6c2a67ce71f
ArjunVarmaLeeds <sc22a2v@leeds.ac.uk>
2026-04-06 21:01:33 +0100

    Setup search module blueprint

---
+++ src/search.py
@@ -0,0 +1,22 @@
+from typing import Dict, List
+import re
+import math
+
+class Search:
+    def __init__(self, index: Dict):
+        pass
+
+    def normalize(self, word: str) -> str:
+        pass
+    
+    def process_query(self, text: str):
+        pass
+
+    def print_word(self, word: str) -> Dict:
+        pass
+
+    def find(self, query: List[str]) -> List[str]:
+        pass
+
+    def find_with_ranking(self, query: List[str]) -> List[tuple]:
+        pass
