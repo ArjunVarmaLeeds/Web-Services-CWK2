93b5cd77ad1eac7b86788b26cd63e17d5d2d0c7b
ArjunVarmaLeeds <sc22a2v@leeds.ac.uk>
2026-04-06 20:47:39 +0100

    Setup indexer blueprint

---
+++ src/indexer.py
@@ -0,0 +1,18 @@
+import json
+import re
+
+class Indexer:
+    def __init__(self):
+        pass
+
+    def tokenize(self, text: str):
+        pass
+    
+    def add_document(self, url: str, text: str):
+        pass
+
+    def build_index(self, crawler_data):
+        pass
+    
+    def save_index(self, filename):
+        pass
