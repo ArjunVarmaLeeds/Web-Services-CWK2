ef560d8d442f5bcf39fe744e72a5bd88e6bb06f1
ArjunVarmaLeeds <sc22a2v@leeds.ac.uk>
2026-04-06 20:48:55 +0100

    Implemented inserting to index function

--- src/indexer.py
+++ src/indexer.py
@@ -3,13 +3,37 @@ import re
 
 class Indexer:
     def __init__(self):
-        pass
+        self.index = {}
 
     def tokenize(self, text: str):
-        pass
+        return re.findall(r"\b\w+\b", text.lower())    
     
     def add_document(self, url: str, text: str):
-        pass
+        """
+        Indexing format
+        {
+        "word": {
+                "page_url": {
+                    "frequency": int,
+                    "positions": [int, int]
+                }
+            }
+        }
+        """
+        words = self.tokenize(text)
+
+        for position, word in enumerate(words):
+            if word not in self.index:
+                self.index[word] = {}
+
+            if url not in self.index[word]:
+                self.index[word][url] = {
+                    "frequency": 0,
+                    "positions": []
+                }
+
+            self.index[word][url]["frequency"] += 1
+            self.index[word][url]["positions"].append(position)
 
     def build_index(self, crawler_data):
         pass
